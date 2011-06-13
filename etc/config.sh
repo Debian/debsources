9122cbf537e8bef0b9317738474c7ae1c23c84c2
Stefano Zacchiroli <zack@upsilon.cc>
2011-06-13 12:15:54 +0200

    configuration framework to share options among binaries

---
+++ etc/config.sh
@@ -0,0 +1,13 @@
+# source this file to get common shell (bash) configuration variables
+
+# Unix group owning sources.d.o archive.  We need to set this explicitly after
+# each .dsc extraction, since dpkg-source -x insists in not inheriting group
+# from (+s) dirs.
+gid="sourcesdo"
+
+# Directories where sources.d.o binaries can be found.
+bindir="$root/bin"
+
+# Directories where a Debian source mirror can be found.
+mirror_dir="/srv/debian-mirror"
+
