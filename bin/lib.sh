2a5db089119696792d976b6fd8ef04bd6f827d82
Stefano Zacchiroli <zack@upsilon.cc>
2013-05-21 16:13:48 +0200

    move stat printing to lib.sh
    rationale: would allow to use them from multiple scripts

--- bin/lib.sh
+++ bin/lib.sh
@@ -21,3 +21,32 @@ err() {
     echo "E: $msg"
     exit $code
 }
+
+
+stats_period=1000
+stats_label=""
+stats_total=""
+stats_count=""
+
+stats_init() {
+    stats_label="$1"
+    stats_total="$2"
+    stats_count=0
+}
+
+stats_print() {
+    if [ "$verbose" = "1" ] ; then
+	msg="heartbeat: $stats_count"
+	if [ -n "$stats_total" ] ; then
+	    msg="$msg / $stats_total"
+	fi
+        info "$msg"
+    fi
+}
+
+stats_tick() {
+    stats_count=$[$stats_count+1]
+    if [ $[$stats_count % $stats_period] -eq 0 ] ; then
+        stats_print
+    fi
+}
