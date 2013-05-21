fd3d5753cfd27cd74764069339f32436dbbc238e
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-05-22 01:09:42 +0200

    refactoring of sources2db.py: more memory, less db requests, less time

--- web/flaskapp/app/views.py
+++ web/flaskapp/app/views.py
@@ -3,8 +3,12 @@ from flask import render_template
 from app import app, db
 from models import Package, Version
 
+from flask.ext.sqlalchemy import BaseQuery
+
 @app.route('/') # navigation
 def index():
-    packages = db.session.query(Package).all()
+    #a=BaseQuery(Package, session=db.session).paginate(1).items
+    #packages = db.session.query(Package).paginate(1, 10, False).items#all()
+    packages = db.session.query(Package).order_by(Package.name).limit(50)
     return render_template('index.html',
-                           packages=packages[:50])
+                           packages=packages)#packages[:])
--- web/flaskapp/scripts/sources2db.py
+++ web/flaskapp/scripts/sources2db.py
@@ -11,45 +11,11 @@ sys.path.insert(0,parentdir)
 
 from models import Base, Package, Version
 
-class BadFileError(ValueError): pass
-class EmptyFileError(BadFileError): pass
-
 def get_engine_session(url):
     engine = create_engine(url, echo=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     return engine, session
-
-class GroupVersions(object):
-    """ given a sources file, returns an iterator like:
-    (package1, [version1, version2, ...])
-    (package2, [version1, version2, ...])
-    """
-    def __init__(self, file):
-        self.file = open(file)
-    def __iter__(self):
-        cols = self.file.readline().split()
-        self.package = cols[0]
-        self.version = [cols[1]]
-        return self
-    def next(self):
-        if self.file.closed:
-            raise StopIteration
-        while(True):
-            try:
-                cols = self.file.readline().split()
-                if cols[0] == self.package:
-                    self.version.append(cols[1])
-                else:
-                    ret_p = self.package
-                    ret_v = self.version
-                    self.package = cols[0]
-                    self.version = [cols[1]]
-                    return (ret_p, ret_v)
-            except IndexError:
-                self.file.close()
-                return (self.package, self.version)
-
 
 def sources2db(sources,  db_url, drop=False):
     engine, session = get_engine_session(url)
@@ -58,32 +24,33 @@ def sources2db(sources,  db_url, drop=False):
         Base.metadata.drop_all(engine)
         Base.metadata.create_all(engine)
     
-    # now we update the db
-    for (package, versions) in GroupVersions(sources):
-        # if the package doesn't exist yet, we add it,
-        # get its id, and add the versions
-        if not(session.query(exists().where(
-                    Package.name == package)).scalar()):
-            session.add(Package(package))
-            session.commit()
-            package_id = session.query(Package).filter(
-                Package.name == package).first().id
-            for version in versions:
-                version = Version(version)
-                version.package_id = package_id
-                session.add(version)
-        # else we get its id and add the inexisting versions
-        else:
-            package_id = session.query(Package).filter(
-                Package.name == package).first().id
-            for version in versions:
-                version = Version(version)
-                if not(session.query(exists().where(and_(
-                                Version.vnumber == version.vnumber,
-                                Version.package_id == package_id)))):
-                    session.add(version)
-    session.commit()
-            
+    # v2
+    # first we create the set of all packages
+    packages = set()
+    with open(sources) as sfile:
+        for line in sfile:
+            packages.add(line.split()[0])
+    # now the associated dict to work with execute
+    packlist = []
+    for p in packages:
+        packlist.append(dict(name=p))
+    packages = None
+    Package.__table__.insert(bind=engine).execute(packlist)
+    # we get the packages list along with their ids
+    packages = session.query(Package).all()
+    # we build the dict (package1: id1, ...)
+    packids = dict()
+    for p in packages:
+        packids[p.name] = p.id
+    # we build the list [dict(vnumber=.., package_id=..), ..]
+    versions = []
+    with open(sources) as sfile:
+        for line in sfile:
+            cols = line.split()
+            versions.append(dict(vnumber=cols[1],
+                                 package_id=packids[cols[0]]))
+    packids = None
+    Version.__table__.insert(bind=engine).execute(versions)
             
 
 if __name__ == "__main__":
