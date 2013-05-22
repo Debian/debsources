53e13cb2e365015690f1fc073dddef964d77198d
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-05-22 09:52:18 +0200

    sources2db: utilisation of list comprehensions (more readable)

--- web/flaskapp/scripts/sources2db.py
+++ web/flaskapp/scripts/sources2db.py
@@ -1,5 +1,6 @@
 import argparse
 import sys, os
+import time
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -25,35 +26,32 @@ def sources2db(sources,  db_url, drop=False):
         Base.metadata.create_all(engine)
     
     # v2
-    # first we create the set of all packages
+    # first we create the set of all packages and the list of (pack, vers)
     packages = set()
+    versions = []
     with open(sources) as sfile:
         for line in sfile:
-            packages.add(line.split()[0])
+            cols = line.split() # package, version, other stuff
+            packages.add(cols[0])
+            versions.append((cols[0], cols[1]))
     # now the associated dict to work with execute
-    packlist = []
-    for p in packages:
-        packlist.append(dict(name=p))
-    packages = None
-    Package.__table__.insert(bind=engine).execute(packlist)
+    Package.__table__.insert(bind=engine).execute(
+        [dict(name=p) for p in packages]
+        )
     # we get the packages list along with their ids
     packages = session.query(Package).all()
     # we build the dict (package1: id1, ...)
     packids = dict()
     for p in packages:
         packids[p.name] = p.id
-    # we build the list [dict(vnumber=.., package_id=..), ..]
-    versions = []
-    with open(sources) as sfile:
-        for line in sfile:
-            cols = line.split()
-            versions.append(dict(vnumber=cols[1],
-                                 package_id=packids[cols[0]]))
-    packids = None
-    Version.__table__.insert(bind=engine).execute(versions)
+    # finally the versions dict to work with execute
+    Version.__table__.insert(bind=engine).execute(
+        [dict(vnumber=b, package_id=packids[a]) for a, b in versions]
+        )
             
 
 if __name__ == "__main__":
+    start_time = time.time()
     parser = argparse.ArgumentParser(description="Updates a SQLite base from \
     a sources.txt file")
     parser.add_argument("sqlite_file",
@@ -73,3 +71,5 @@ if __name__ == "__main__":
     #os.environ['PYTHONINSPECT'] = 'True'
 
     sources2db(args.sources, url, drop=args.drop)
+    print()
+    print(time.time() - start_time)
