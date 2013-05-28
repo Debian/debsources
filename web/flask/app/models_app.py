a73b47961b6db8d51834b46349fd10a518e3406a
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-05-28 12:01:55 +0200

    app: refactoring: functions deplaced from views to model_apps, creation of Location, Directory and SourceFile classes

--- web/flask/app/models_app.py
+++ web/flask/app/models_app.py
@@ -1,5 +1,11 @@
-from app import db
+from app import app, db
 import models
+from modules.packages_prefixes import packages_prefixes
+from modules.sourcecode import SourceCodeIterator
+
+from flask import url_for
+
+import os
 
 class Package_app(models.Package, db.Model):
     pass
@@ -7,3 +13,119 @@ class Package_app(models.Package, db.Model):
 class Version_app(models.Version, db.Model):
     pass
 
+class Location(object):
+    """ a location in a package, can be a directory or a file """
+    def __init__(self, package, version=None, path_to=None):
+        self.package = package
+        self.version = version or ""
+        self.path_to = path_to or ""
+        
+        self.sources_path = os.path.join(app.config['SOURCES_FOLDER'],
+                                         self.package, self.version,
+                                         self.path_to)
+        #self.sources_path_server = os.path.join(app.config['SOURCES_SERVER'],
+        #                                        package, version, path_to)
+    
+    def isdir(self):
+        """ True if self is a directory, False if it's not """
+        return os.path.isdir(self.sources_path)
+    
+    def isfile(self):
+        """ True if sels is a file, False if it's not """
+        return os.path.isfile(self.sources_path)
+    
+    def get_path_links(self):
+        """
+        returns the path hierarchy with urls, to use with 'You are here:'
+        [(name, url(name)), (...), ...]
+        """
+        pathl = []
+        pathl.append((self.package, url_for('source', package=self.package)))
+        
+        if self.version != "":
+            pathl.append((self.version, url_for('source', package=self.package,
+                                                version=self.version)))
+        if self.path_to != "":
+            prev_path = ""
+            for p in self.path_to.split('/'):
+                pathl.append((p, url_for('source', package=self.package,
+                                         version=self.version,
+                                         path_to=prev_path+p)))
+                prev_path += p+"/"
+        return pathl
+
+class Directory(Location):
+    """ a folder in a package """
+    def _sub_url(self, subfile):
+        """ returns the URL of a sub file/folder in this directory """
+        if self.version == "":
+            return url_for('source', package=self.package, version=subfile)
+        elif self.path_to == "":
+            return url_for('source', package=self.package,
+                           version=self.version, path_to=subfile)
+        else:
+            return url_for('source', package=self.package,
+                           version=self.version,
+                           path_to=self.path_to+"/"+subfile)
+    
+    def get_subdirs(self):
+        """ returns the list of the subfolders along with their URLs """
+        return sorted((f, self._sub_url(f))
+                      for f in os.listdir(self.sources_path)
+                      if os.path.isdir(os.path.join(self.sources_path, f)))
+    
+    def get_subfiles(self):
+        """ returns the list of the subfiles along with their URLs """
+        return sorted((d, self._sub_url(d))
+                      for d in os.listdir(self.sources_path)
+                      if os.path.isfile(os.path.join(self.sources_path, d)))
+    
+    def is_top_folder(self):
+        """ True if this is a top folder of a package, False otherwise """
+        return self.version == ""
+
+class SourceFile(Location):
+    """ a source file in a package """
+    def __init__(self, package, version, path_to, highlight, msg):
+        super(SourceFile, self).__init__(package, version, path_to)
+        self.highlight = highlight
+        self.msg = msg
+        self.number_of_lines = None
+        self.code = SourceCodeIterator(self.sources_path, self.highlight)
+    
+    def get_msgdict(self):
+        """
+        returns a dict(position=, title=, message=) generated from
+        the string message (position:title:message)
+        """
+        if self.msg is None: return dict()
+        msgsplit = self.msg.split(':')
+        msgdict = dict()
+        try:
+            msgdict['position'] = int(msgsplit[0])
+        except ValueError:
+            msgdict['position'] = 1
+        try:
+            msgdict['title'] = msgsplit[1]
+        except IndexError:
+            msgdict['title'] = ""
+        try:
+            msgdict['message'] = ":".join(msgsplit[2:])
+        except IndexError:
+            msgdict['message'] = ""
+        return msgdict
+    
+    def get_number_of_lines(self):
+        if self.number_of_lines is not None:
+            return number_of_lines
+        number_of_lines = 1
+        with open(self.sources_path) as sfile:
+            for line in sfile: number_of_lines += 1
+        return number_of_lines
+    
+    def get_code(self):
+        return self.code
+
+def get_packages_prefixes():
+    """ returns the packages prefixes (a, b, ..., liba, libb, ..., y, z) """
+    return packages_prefixes
--- web/flask/app/views.py
+++ web/flask/app/views.py
@@ -1,34 +1,13 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, safe_join
 
 from app import app
-from models_app import Package_app, Version_app
+from models_app import Package_app, Version_app, get_packages_prefixes, \
+    Location, Directory, SourceFile
 from forms import SearchForm
-
-def get_letters():
-    from modules.packages_prefixes import packages_prefixes
-    return packages_prefixes
-
-def get_path_links(package, version="", path_to=""):
-    """
-    returns the path hierarchy with urls, to use with 'You are here:'
-    """
-    pathl = []
-    pathl.append((package, url_for('source', package=package)))
-    if version != "":
-        pathl.append((version, url_for('source', package=package,
-                                       version=version)))
-    if path_to != "":
-        prev_path = ""
-        for p in path_to.split('/'):
-            pathl.append((p, url_for('source', package=package,
-                                     version=version,
-                                     path_to=prev_path+p)))
-            prev_path += p+"/"
-    return pathl
 
 @app.context_processor # variables needed by "base.html" skeleton
 def skeleton_variables():
-    return dict(packages_prefixes = get_letters(),
+    return dict(packages_prefixes = get_packages_prefixes(),
                 searchform = SearchForm())
 
 @app.errorhandler(404)
@@ -39,22 +18,19 @@ def page_not_found(e):
 def doc():
     return render_template('doc.html', url_prefix="http://example.net")
 
-@app.route('/', methods=['POST', 'GET']) # navigation
-@app.route('/nav/', methods=['POST', 'GET'])
+@app.route('/')
+@app.route('/nav/') # navigation
 def index():
-    #searchform = SearchForm()
-    #if searchform.validate_on_submit():
-    #    return redirect(url_for("search",
-    #                            packagename=searchform.packagename.data))
     return render_template('index.html')
 
-@app.route('/nav/search/', methods=['POST'])
+@app.route('/nav/search/', methods=['GET', 'POST'])
 def receive_search():
     searchform = SearchForm(request.form)
     if searchform.validate_on_submit():
         return redirect(url_for("search",
                                 packagename=searchform.packagename.data))
     else:
+        # we return the form, to display the errors
         return render_template('index.html', searchform=searchform)
 
 @app.route('/nav/search/<packagename>/')
@@ -80,7 +56,7 @@ def list(page=1):
 @app.route('/nav/letter/')
 @app.route('/nav/letter/<letter>')
 def letter(letter='a'):
-    if letter in get_letters():
+    if letter in get_packages_prefixes():
         packages = Package_app.query.filter(
             Package_app.name.startswith(letter)).order_by(Package_app.name)
         return render_template("letter.html",
@@ -93,80 +69,36 @@ def letter(letter='a'):
 @app.route('/src/<package>/')
 @app.route('/src/<package>/<version>/')
 @app.route('/src/<package>/<version>/<path:path_to>/', methods=['POST', 'GET'])
-def source(package, version="", path_to=None):
-    #if version == "": # we list the versions for this package
-    #    return render_template("source_package.html") # todo
+def source(package, version=None, path_to=None):
+    location = Location(package, version, path_to)
     
-    import os
-    from flask import safe_join
-    if path_to is None:
-        path_to = ""
-    sources_path = os.path.join(app.config['SOURCES_FOLDER'],
-                                package, version, path_to)
-    sources_path_server = os.path.join(app.config['SOURCES_SERVER'],
-                                       package, version, path_to)
-
-    if os.path.isdir(sources_path): # we list the files in this folder
-        def quickurl(f):
-            if version == "":
-                return url_for('source', package=package, version=f)
-            elif path_to == "":
-                return url_for('source', package=package,
-                               version=version, path_to=f)
-            else:
-                return url_for('source', package=package,
-                               version=version,
-                               path_to=path_to+"/"+f)
-        
-        files = sorted((f, quickurl(f)) for f in os.listdir(sources_path)
-                       if os.path.isfile(os.path.join(sources_path, f)))
-
-        dirs = sorted((d, quickurl(d)) for d in os.listdir(sources_path)
-                      if os.path.isdir(os.path.join(sources_path, d)))
+    if location.isdir(): # it's a folder, we list its content
+        location = Directory(package, version, path_to)
         
         return render_template("source_folder.html",
-                               files=files, dirs=dirs,
-                               pathl=get_path_links(package, version, path_to),
-                               parentfolder=(version != ""))
+                               files=location.get_subfiles(),
+                               dirs=location.get_subdirs(),
+                               pathl=location.get_path_links(),
+                               parentfolder=not(location.is_top_folder()))
                                  # we want '..', except for a package file
     
-    elif os.path.exists(sources_path): # it's a file, we return the source code
-        def msg_strtodict(msg):
-            if msg is None: return dict()
-            msgsplit = msg.split(':')
-            msgdict = dict()
-            try:
-                msgdict['position'] = int(msgsplit[0])
-            except ValueError:
-                msgdict['position'] = 1
-            try:
-                msgdict['title'] = msgsplit[1]
-            except IndexError:
-                msgdict['title'] = ""
-            try:
-                msgdict['message'] = ":".join(msgsplit[2:])
-            except IndexError:
-                msgdict['message'] = ""
-            return msgdict
-
+    elif location.isfile(): # it's a file, we display the source code
         try:
-            hl = request.args.get('hl')
+            highlight = request.args.get('hl')
         except (KeyError, ValueError, TypeError):
             hl = None
         try:
             msg = request.args.get('msg')
-            msg = msg_strtodict(msg)
         except (KeyError, ValueError, TypeError):
             msg = None
+            
+        location = SourceFile(package, version, path_to, highlight, msg)
         
-        nlines = 1
-        with open(sources_path) as sfile:
-            for line in sfile: nlines += 1 # counts the total number of lines
-        from modules.sourcecode import SourceCodeIterator
         return render_template("source_file.html",
-                               code = SourceCodeIterator(sources_path,
-                                                         hl=hl),
-                               nlines=nlines, msg=msg,
-                               pathl=get_path_links(package, version, path_to))
+                               code = location.get_code(),
+                               nlines=location.get_number_of_lines(),
+                               msg=location.get_msgdict(),
+                               pathl=location.get_path_links())
+    
     else: # 404
         return render_template('404.html'), 404
