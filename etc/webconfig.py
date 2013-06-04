7f42d1ca32028eb4d68673923769ac0290679cae
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-06-04 23:24:17 +0200

    webconfig.py: use of ROOT_FOLDER, and documentation of each variable

--- etc/webconfig.py
+++ etc/webconfig.py
@@ -1,15 +1,24 @@
+###################################
+# Debsources webapp configuration #
+###################################
+
 # this file is loaded as a Python module from flask/app/__init__.py using a
 # relative path ('../../../etc/appconfig.py')
 
+# the domain of the webapp, used in documentation:
 DOMAIN = "sources.debian.net"
 
-#SECRET_KEY = 'SecretKeyForSessionSigning' # not in use
+# the secret key for session signing:
+# SECRET_KEY = 'some_hash' # not in use
 
+# CSRF token for WTForms:
 # we don't have any form which writes data
 CSRF_ENABLED = False
-#CSRF_SESSION_KEY = "somethingimpossibletoguess" # not in use
 
-# the place where the browser can GET the highlight.js library (JS + CSS)
+# related session key:
+# CSRF_SESSION_KEY = "some_hash" # not in use
+
+# the place where the browser can GET the highlight.js library (JS + CSS):
 HIGHLIGHT_JS_FOLDER = "/javascript/highlight"
 
 # CSS style for highlight.js
@@ -17,24 +26,40 @@ HIGHLIGHT_JS_FOLDER = "/javascript/highlight"
 # HIGHLIGHT_STYLE = "default"
 HIGHLIGHT_STYLE = "googlecode"
 
-# the place where the icons are 
+# the place where the tango icons (or other icons) are :
+ICONS_FOLDER = "foo" # TODO
+
+
 
 ### PROD ###
 
+# the root folder of the application, normally the "debsources" location:
+ROOT_FOLDER = "/srv/debsources/"
+
+# /!\ never set this to True in production:
 DEBUG = False
+
+# echoes or not the SQL requests to stdout (can be logged with Apache):
 SQLALCHEMY_ECHO = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:////srv/debsources/cache/sources.sqlite'
-SOURCES_FOLDER = "/srv/debsources/sources/" # for listing folders and files
-SOURCES_STATIC = "/data" # for external raw links
-MODELS_FOLDER = "/srv/debsources/web"
-LAST_UPDATE_FILE = "/srv/debsources/cache/last-update"
+
+# the uri of the database
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + \
+    os.path.join(ROOT_FOLDER, "cache/sources.sqlite")
+
+# where the sources are on the disk, for listing folders and files:
+SOURCES_FOLDER = os.path.join(ROOT_FOLDER, "sources")
+
+# where the sources are accessible for a browser, for raw links:
+SOURCES_STATIC = "/data"
+
+# where the models.py file is stored:
+MODELS_FOLDER = os.path.join(ROOT_FOLDER, "web")
+
+# where the last-update is stored, to be displayed in the footer:
+LAST_UPDATE_FILE = os.path.join(ROOT_FOLDER, "cache/last-update")
 
 
 ### DEV ###
 
 # DEBUG = True
 # SQLALCHEMY_ECHO = True
-# SQLALCHEMY_DATABASE_URI = 'sqlite:////home/matthieu/work/debian/debsources/web/app.db'
-# SOURCES_FOLDER = "/var/www/debsources/app/static/data/" # for listing folders and files
-# SOURCES_STATIC = "/static/data" # for external raw links
-# MODELS_FOLDER = "/home/matthieu/work/debian/debsources/web/"
