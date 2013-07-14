9ca2965cf181e774cd03c680c2b7535f33dae812
Stefano Zacchiroli <zack@upsilon.cc>
2013-07-14 17:03:16 +0200

    hashutil: new Python module to take sha{1,256}sums of debsources file
    bare bone, but as efficient as coreutils implementation in realistic benchmarks

---
+++ web/hashutil.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2013  Stefano Zacchiroli <zack@upsilon.cc>
+#
+# This file is part of Debsources.
+#
+# Debsources is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import hashlib
+
+# should be a multiple of 64 (sha1/sha256's block size)
+# FWIW coreutils' sha1sum uses 32768
+HASH_BLOCK_SIZE = 32768
+
+
+def sha1sum(path):
+    m = hashlib.sha1()
+    with open(path) as f:
+        while True:
+            chunk = f.read(HASH_BLOCK_SIZE)
+            if not chunk:
+                break
+            m.update(chunk)
+    return m.hexdigest()
+
+
+def sha256sum(path):
+    m = hashlib.sha256()
+    with open(path) as f:
+        while True:
+            chunk = f.read(HASH_BLOCK_SIZE)
+            if not chunk:
+                break
+            m.update(chunk)
+    return m.hexdigest()
