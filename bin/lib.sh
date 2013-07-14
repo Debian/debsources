fc40e05565a4f3c624c9358f80053059ebb759c6
Stefano Zacchiroli <zack@upsilon.cc>
2013-07-14 15:04:13 +0200

    lib.sh: support backslash escapes in messages
    e.g. in functions info(), warn(), err()

--- bin/lib.sh
+++ bin/lib.sh
@@ -22,19 +22,19 @@ verbose="1"
 info() {
     local msg="$1"
     if [ "$verbose" = "1" ] ; then
-	echo "I: $msg"
+	echo -e "I: $msg"
     fi
 }
 
 warn() {
     local msg="$1"
-    echo "W: $msg"
+    echo -e "W: $msg"
 }
 
 err() {
     local msg="$1"
     local code="$2"
-    echo "E: $msg"
+    echo -e "E: $msg"
     exit $code
 }
 
