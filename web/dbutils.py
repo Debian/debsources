8988e8bdfe41444339c2702a1caff9de881d0481
Stefano Zacchiroli <zack@upsilon.cc>
2013-08-01 11:58:56 +0200

    update-ng: better logging, avoid a priori filtering
    Gotcha: logging.basicConfig sets log *filtering*, meaning that additional
    handlers cannot be more verbose than the initially set log level. Worked around
    that by logging everything (logging.DEBUG and above) by default, then setting
    the desired verbosity in the default log handler (logfile) and console log
    handlers.
    
    Also: revamp log messages with a more fine grained distinction between INFO-
    and DEBUG-level messages.

--- web/dbutils.py
+++ web/dbutils.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import exists
@@ -26,6 +28,7 @@ from models import Base, Package, Version, VCS_TYPES
 def add_package(session, pkg):
     """Add a package (= debmirror.SourcePackage) to the Debsources db
     """
+    logging.debug('add to db %s...' % pkg)
     package = session.query(Package).filter_by(name=pkg['package']).first()
     if not package:
         package = Package(pkg['package'])
@@ -52,6 +55,7 @@ def add_package(session, pkg):
 def rm_package(session, db_version):
     """Remove a package (= debmirror.SourcePackage) from the Debsources db
     """
+    logging.debug('remove from db %s...' % pkg)
     session.delete(db_version)
     if not db_version.package.versions:
         # just removed last version, get rid of package too
