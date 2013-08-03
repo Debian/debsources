363953765ef9b37f962cd45bc49649b78aa7f1b0
Stefano Zacchiroli <zack@upsilon.cc>
2013-08-03 18:24:37 +0200

    checksums: store persistently sha256 in pkg.checksums file
    to decouple the checksum fs pass from the corresponding db pass

--- bin/plugins/hook_checksums.py
+++ bin/plugins/hook_checksums.py
@@ -26,6 +26,22 @@ from models import Checksum
 
 conf = None
 
+sums_path = lambda pkgdir: pkgdir + '.checksums'
+
+
+def parse_checksums(path):
+    """parse sha256 checksums from a file in SHA256SUM(1) format
+
+    i.e. each line is "SHA256  PATH\n"
+
+    yield (sha256, path) pairs
+    """
+    with open(path) as checksums:
+        for line in checksums:
+            sha256 = line[0:64]
+            path = line[66:]
+            yield (sha256, path)
+
 
 def walk_pkg_files(pkgdir):
     if isinstance(pkgdir, unicode):
@@ -43,41 +59,52 @@ def add_package(session, pkg, pkgdir):
     global conf
     logging.debug('add-package %s' % pkg)
 
-    if not 'hooks.db' in conf['passes']:
-        return
+    sumsfile = sums_path(pkgdir)
+    sumsfile_tmp = sumsfile + '.new'
 
-    version = dbutils.lookup_version(session, pkg['package'], pkg['version'])
-    for path in walk_pkg_files(pkgdir):
-        if os.path.islink(path):
-            # do not checksum symlinks, if they are not dangling/external we
-            # will checksum their target anyhow
-            continue
-        sha256 = hashutil.sha256sum(path)
-        relpath = os.path.relpath(path, pkgdir)
-        checksum = session.query(Checksum) \
-                          .filter_by(version_id=version.id,
-                                     path=relpath,
-                                     sha256=sha256) \
-                          .first()
-        if checksum:
-            break # ASSUMPTION: if *a* checksum of this package has already
-                  # been added to the db in the past, then *all* of them have,
-                  # as additions are part of the same transaction
-        checksum = Checksum(version, relpath, sha256)
-        session.add(checksum)
+    if 'hooks.fs' in conf['passes']:
+        if not os.path.exists(sumsfile): # compute checksums only if needed
+            with open(sumsfile_tmp, 'w') as out:
+                for path in walk_pkg_files(pkgdir):
+                    if os.path.islink(path):
+                        # do not checksum symlinks, if they are not dangling /
+                        # external we will checksum their target anyhow
+                        continue
+                    sha256 = hashutil.sha256sum(path)
+                    relpath = os.path.relpath(path, pkgdir)
+                    out.write('%s  %s\n' % (sha256, relpath))
+            os.rename(sumsfile_tmp, sumsfile)
+
+    if 'hooks.db' in conf['passes']:
+        version = dbutils.lookup_version(session, pkg['package'], pkg['version'])
+        for (sha256, relpath) in parse_checksums(sumsfile):
+            checksum = session.query(Checksum) \
+                              .filter_by(version_id=version.id,
+                                         path=relpath,
+                                         sha256=sha256) \
+                              .first()
+            if checksum:
+                break # ASSUMPTION: if *a* checksum of this package has already
+                      # been added to the db in the past, then *all* of them have,
+                      # as additions are part of the same transaction
+            checksum = Checksum(version, relpath, sha256)
+            session.add(checksum)
 
 
 def rm_package(session, pkg, pkgdir):
     global conf
     logging.debug('rm-package %s' % pkg)
 
-    if not 'hooks.db' in conf['passes']:
-        return
+    if 'hooks.fs' in conf['passes']:
+        sumsfile = sums_path(pkgdir)
+        if os.path.exists(sumsfile):
+            os.unlink(sumsfile)
 
-    version = dbutils.lookup_version(session, pkg['package'], pkg['version'])
-    session.query(Checksum) \
-           .filter_by(version_id=version.id) \
-           .delete()
+    if 'hooks.db' in conf['passes']:
+        version = dbutils.lookup_version(session, pkg['package'], pkg['version'])
+        session.query(Checksum) \
+               .filter_by(version_id=version.id) \
+               .delete()
 
 
 def debsources_main(debsources):
