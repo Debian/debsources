bd94e732c78a1224cdd19572b0ffcc9eb9dd8ef1
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-08-08 00:48:37 +0200

    fix: run.py: add sys.insert()

--- web/flask/run.py
+++ web/flask/run.py
@@ -1,4 +1,9 @@
 if __name__ == "__main__":
+    import sys, os
+    
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(PROJECT_DIR)
+    
     from app import app
     app.run(debug=True)
 
