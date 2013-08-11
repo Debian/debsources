d86149a020844a00de06fb52b24e862494329901
Stefano Zacchiroli <zack@upsilon.cc>
2013-08-11 14:28:47 +0200

    clean up: remove webconfig_testing.py
    no longer used

--- etc/webconfig_testing.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ROOT_FOLDER = "/home/matthieu/work/debian/debsources/"
-SOURCES_STATIC = "/static/data"
-MODELS_FOLDER = os.path.join(ROOT_FOLDER, "web")
-LAST_UPDATE_FILE = os.path.join(ROOT_FOLDER, "cache/last-update")
-TESTING = True
-SOURCES_FOLDER = "/home/matthieu/work/debian/debsources/web/flask/tests/sources"
-SQLALCHEMY_DATABASE_URI = "postgresql://matthieu:matthieu@localhost:5432/debsources_test"
