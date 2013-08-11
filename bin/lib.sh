f88a2e7a45f0cfd5c8baabcd5dc08f7863685ffe
Stefano Zacchiroli <zack@upsilon.cc>
2013-08-11 17:03:22 +0200

    update: improve logging and lockfile handling in the driver script
    note: moved lockfile handling away from bin/update-debsources, it really
    doesn't belong there

---
+++ bin/lib.sh
@@ -0,0 +1,37 @@
+# Copyright (C) 2011-2013  Stefano Zacchiroli <zack@upsilon.cc>
+#
+# This file is part of Debsources.
+#
+# Debsources is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+verbose="1"
+
+info() {
+    local msg="$1"
+    if [ "$verbose" = "1" ] ; then
+	echo -e "I: $msg" >> $logfile
+    fi
+}
+
+warn() {
+    local msg="$1"
+    echo -e "W: $msg" >> $logfile
+}
+
+err() {
+    local msg="$1"
+    local code="$2"
+    echo -e "E: $msg" >> $logfile
+    exit $code
+}
