f8a100e6a0f0ebedf61d37c6ac2c26d110ac1884
Stefano Zacchiroli <zack@upsilon.cc>
2014-02-21 22:23:06 +0100

    run_shell.py: parse conf file and pre-populate session

--- python/run_shell.py
+++ python/run_shell.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 
 import os
+import readline
+import rlcompleter
+
 from pprint import pprint
 
-import rlcompleter
-import readline
-readline.parse_and_bind("tab: complete")
+import mainlib
+import sqla_session
 
-
-#from flask import *
 from app import *
 
+PYDIR = os.path.dirname(os.path.abspath(__file__))
+ROOTDIR = os.path.dirname(PYDIR)
+ETCDIR = os.path.join(ROOTDIR, 'etc')
+CONFFILE = os.path.join(ETCDIR, 'config.ini')
+__alt_conffile = os.path.join(ETCDIR, 'config.local.ini')
+if os.path.exists(__alt_conffile):
+    CONFFILE = __alt_conffile
+
+readline.parse_and_bind("tab: complete")
 os.environ['PYTHONINSPECT'] = 'True'
+
+conf = mainlib.load_configuration(CONFFILE)
+session, engine = sqla_session._get_engine_session(conf['db_uri'])
