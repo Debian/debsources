dfc6b862525ca101ccb207e6144a4c8046c73551
Stefano Zacchiroli <zack@upsilon.cc>
2014-02-25 11:43:36 +0100

    charts: use matplotlib "Agg" backend
    to avoid requiring X connectivity for plotting

--- python/charts.py
+++ python/charts.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from itertools import cycle
