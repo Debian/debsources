c44c8ad13e0ae42e02c6963effca4b47cbace0ae
Matthieu Caneill <matthieu.caneill42@gmail.com>
2014-07-21 22:04:36 -0400

    run_webapp.py: add options parsing (for host and port)

--- python/run_webapp.py
+++ python/run_webapp.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 
+import sys
+import os
+import argparse
+
 if __name__ == "__main__":
-    import sys
-    import os
+    parser = argparse.ArgumentParser(description='Run Flask app, for dev/debug.')
+    parser.add_argument('--ip', type=str, default='127.0.0.1', required=False,
+                        help='Local IP adress')
+    parser.add_argument('--port', type=int, default=5000, required=False,
+                        help='Port in use')
+    args = parser.parse_args()
 
     PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(PROJECT_DIR)
 
     from app import app_wrapper
     app_wrapper.go()
-    app_wrapper.app.run(debug=True)
+    app_wrapper.app.run(debug=True, host=args.ip, port=args.port)
