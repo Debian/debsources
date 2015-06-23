668e363a8fb3ef3e0532b78aad1b739086a065ba
Orestis Ioannou <orestis@oioannou.com>
2015-06-23 12:41:25 +0200

    Add copyright table, historical trends and migration script

---
+++ debsources/migrate/008-to-009.sql
@@ -0,0 +1,14 @@
+CREATE TABLE copyright (
+  id SERIAL NOT NULL,
+  file_id INTEGER NOT NULL,
+  oracle oracles NOT NULL,
+  license VARCHAR,
+  CONSTRAINT copyright_file_id_fkey
+    FOREIGN KEY (file_id) REFERENCES files(id)
+    ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
+CREATE TYPE oracles as ENUM(
+    'debian'
+)
