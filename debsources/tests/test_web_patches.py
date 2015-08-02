ed52b2760d1616bdcaf4a3a4f8764ebd5889b9f6
Orestis Ioannou <orestis@oioannou.com>
2015-08-02 18:11:00 +0200

    Add test for file deltas links in the patch tracker summary

--- debsources/tests/test_web_patches.py
+++ debsources/tests/test_web_patches.py
@@ -73,7 +73,8 @@ class CopyrightTestCase(DebsourcesBaseWebTests, unittest.TestCase):
     def test_package_summary(self):
         rv = self.app.get('/patches/summary/beignet/1.0.0-1/')
         self.assertIn("Enhance debug output", rv.data)
-        self.assertIn("utests/builtin_acos_asin.cpp  |    8 +++++---", rv.data)
+        self.assertIn("utests/builtin_acos_asin.cpp</a> |    8 +++++---",
+                      rv.data)
 
         # test non quilt package
         rv = self.app.get('/patches/summary/cvsnt/2.5.03.2382-3/')
@@ -86,6 +87,11 @@ class CopyrightTestCase(DebsourcesBaseWebTests, unittest.TestCase):
         # highlight inside?
         self.assertIn('hljs.highlightBlock', rv.data)
         self.assertIn('highlight/highlight.min.js"></script>', rv.data)
+
+    def test_file_deltas_links(self):
+        rv = self.app.get('/patches/summary/beignet/1.0.0-1/')
+        self.assertIn('<a href="/src/beignet/1.0.0-1/src/cl_utils.h/">',
+                      rv.data)
 
 
 if __name__ == '__main__':
