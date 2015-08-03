fa77eacde16ccd324a11bf550e7cd2dbf1dfc575
Orestis Ioannou <orestis@oioannou.com>
2015-08-03 08:11:50 +0200

    Test 404 when at license/package/version/foo

--- debsources/tests/test_web_cp.py
+++ debsources/tests/test_web_cp.py
@@ -323,6 +323,13 @@ class CopyrightTestCase(DebsourcesBaseWebTests, unittest.TestCase):
                    "rce.org/licenses/GPL-3.0\">GPL-3+</a>"
         self.assertIn(synopsis, rv.data)
 
+    def test_license_404(self):
+        rv = self.app.get("/copyright/license/gnubg/1.02.000-2/foo",
+                          follow_redirects=True)
+        self.assertIn('other versions of this package are available', rv.data)
+        link = '<a href="/copyright/license/gnubg/0.90+20091206-4/">'
+        self.assertIn(link, rv.data)
+
 
 if __name__ == '__main__':
     unittest.main(exit=False)
