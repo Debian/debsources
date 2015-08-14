650f1ac3b284a7acc59d2db986083a8c8cfcb6d1
Orestis Ioannou <orestis@oioannou.com>
2015-08-14 23:10:32 +0200

    Delete render.py and move functions in license helper
    The work done in render was not a rendering neither a parsing
    so it was decided to add the relevant functions in the license
    helper module

--- debsources/license_helper.py
+++ debsources/license_helper.py
@@ -11,26 +11,77 @@
 from __future__ import absolute_import
 import io
 import logging
+import re
 
 from flask import url_for
 from debian import copyright
 
 from debsources.navigation import Location, SourceFile
-from debsources.excepts import (FileOrFolderNotFound,
-                                InvalidPackageOrVersionError)
+
 # import debsources.query as qry
+
+
+Licenses = {
+    r'Apache-2(\.0)?': 'http://opensource.org/licenses/Apache-2.0',
+    r'Apache-1(\.0)?': 'http://opensource.org/licenses/Apache-1.0',
+    r'LGPL-2(\.1)?': 'http://opensource.org/licenses/LGPL-2.1',
+    r'LGPL-3(\.0)?': 'http://opensource.org/licenses/LGPL-3.0',
+    r'^GPL-2(\+)?': 'http://opensource.org/licenses/GPL-2.0',
+    r'^GPL-3\+?': 'http://opensource.org/licenses/GPL-3.0',
+    r'^GPL-1': 'http://opensource.org/licenses/GPL-1.0',
+    r'MPL-2(\.0)?': 'http://opensource.org/licenses/MPL-2.0',
+    r'CDDL(-)?': 'http://opensource.org/licenses/CDDL-1.0',
+    r'BSD-4-clause': 'http://spdx.org/licenses/BSD-4-Clause',
+    r'BSD-3-clause': 'http://opensource.org/licenses/BSD-3-Clause',
+    r'BSD-2-clause': 'http://opensource.org/licenses/BSD-2-Clause',
+    r'Artistic(-2)?': 'http://opensource.org/licenses/Artistic-2.0',
+    r'ISC': 'http://opensource.org/licenses/ISC',
+    r'EFL': 'http://opensource.org/licenses/EFL-2.0',
+    r'Python': 'http://opensource.org/licenses/Python-2.0',
+    r'QPL': 'http://opensource.org/licenses/QPL-1.0',
+    r'W3C': 'http://opensource.org/licenses/W3C',
+    r'LPPL': 'http://opensource.org/licenses/LPPL-1.3c',
+    r'Zope': 'http://opensource.org/licenses/ZPL-2.0',
+    r'CC-BY-1\.0': 'http://spdx.org/licenses/CC-BY-1.0',
+    r'CC-BY-2\.0': 'http://spdx.org/licenses/CC-BY-2.0',
+    r'CC-BY-2\.5': 'http://spdx.org/licenses/CC-BY-2.5',
+    r'CC-BY-3\.0': 'http://spdx.org/licenses/CC-BY-3.0',
+    r'CC-BY-SA-1\.0': 'http://spdx.org/licenses/CC-BY-SA-1.0',
+    r'CC-BY-SA-2\.0': 'http://spdx.org/licenses/CC-BY-SA-2.0',
+    r'CC-BY-SA-2\.5': 'http://spdx.org/licenses/CC-BY-SA-2.5',
+    r'CC-BY-SA-3\.0': 'http://spdx.org/licenses/CC-BY-SA-3.0',
+    r'CC-BY-ND-1\.0': 'http://spdx.org/licenses/CC-BY-ND-1.0',
+    r'CC-BY-ND-2\.0': 'http://spdx.org/licenses/CC-BY-ND-2.0',
+    r'CC-BY-ND-2\.5': 'http://spdx.org/licenses/CC-BY-ND-2.5',
+    r'CC-BY-ND-3\.0': 'http://spdx.org/licenses/CC-BY-ND-3.0',
+    r'CC-BY-NC-1\.0': 'http://spdx.org/licenses/CC-BY-NC-1.0',
+    r'CC-BY-NC-2\.0': 'http://spdx.org/licenses/CC-BY-NC-2.0',
+    r'CC-BY-NC-2\.5': 'http://spdx.org/licenses/CC-BY-NC-2.5',
+    r'CC-BY-NC-3\.0': 'http://spdx.org/licenses/CC-BY-NC-3.0',
+    r'CC-BY-NC-SA-1\.0': 'http://spdx.org/licenses/CC-BY-NC-SA-1.0',
+    r'CC-BY-NC-SA-2\.0': 'http://spdx.org/licenses/CC-BY-NC-SA-2.0',
+    r'CC-BY-NC-SA-2\.5': 'http://spdx.org/licenses/CC-BY-NC-SA-2.5',
+    r'CC-BY-NC-SA-3\.0': 'http://spdx.org/licenses/CC-BY-NC-SA-3.0',
+    r'CC-BY-NC-ND-1\.0': 'http://spdx.org/licenses/CC-BY-NC-ND-1.0',
+    r'CC-BY-NC-ND-2\.0': 'http://spdx.org/licenses/CC-BY-NC-ND-2.0',
+    r'CC-BY-NC-ND-2\.5': 'http://spdx.org/licenses/CC-BY-NC-ND-2.5',
+    r'CC-BY-NC-ND-3\.0': 'http://spdx.org/licenses/CC-BY-NC-ND-3.0',
+    r'GFDL-1\.1': 'http://spdx.org/licenses/GFDL-1.1',
+    r'GFDL-1\.2': 'http://spdx.org/licenses/GFDL-1.2',
+    r'GFDL-1\.3': 'http://spdx.org/licenses/GFDL-1.3',
+    r'GFDL-NIV': '#',
+    r'GFDL-1\.0': 'http://spdx.org/licenses/GFDL-1.0',
+    r'CC0': 'http://spdx.org/licenses/CC0-1.0',
+}
 
 
 def get_sources_path(session, package, version, config):
     ''' Creates a sources_path. Returns exception when it arises
     '''
-    try:
-        location = Location(session,
-                            config["SOURCES_DIR"],
-                            config["SOURCES_STATIC"],
-                            package, version, 'debian/copyright')
-    except (FileOrFolderNotFound, InvalidPackageOrVersionError) as e:
-        raise e
+    location = Location(session,
+                        config["SOURCES_DIR"],
+                        config["SOURCES_STATIC"],
+                        package, version, 'debian/copyright')
 
     file_ = SourceFile(location)
 
@@ -45,13 +96,9 @@ def parse_license(sources_path):
     required_fields = ['Format:', 'Files:', 'Copyright:', 'License:']
     d_file = open(sources_path).read()
     if not all(field in d_file for field in required_fields):
-        raise Exception
+        raise copyright.NotMachineReadableError
     with io.open(sources_path, mode='rt', encoding='utf-8') as f:
-        try:
-            c = copyright.Copyright(f)
-            return c
-        except Exception as e:
-            raise e
+        return copyright.Copyright(f)
 
 
 def license_url(package, version):
@@ -66,7 +113,7 @@ def get_license(session, package, version, path, license_path=None):
     # parse license file to get license
     try:
         c = parse_license(license_path)
-    except Exception:
+    except copyright.NotMachineReadableError:
         return None
 
     paragraph = c.find_files_paragraph(path)
@@ -78,5 +125,110 @@ def get_license(session, package, version, path, license_path=None):
                          " missing a license field" % (path, package,
                                                        version))
             return None
+    else:
+        return None
+
+
+def get_copyright_header(copyright):
+    """ Return all the header attributs
+
+    """
+    return copyright.header._RestrictedWrapper__data
+
+
+def parse_copyright_paragraphs_for_html_render(copyright, base_url):
+    """ Returns list of File objects. If `base_url` is provided
+        then it creates links to base_url+glob
+    """
+    paragraphs = []
+    for par in copyright.all_files_paragraphs():
+        globs = []
+        for files in par.files:
+            globs.append({'files': files,
+                          'url': create_url(files, base_url)})
+        try:
+            l = {'license': parse_license_synopsis(copyright,
+                                                   par.license.synopsis),
+                 'text': par.license.text}
+        except AttributeError:
+            l = {'license': None,
+                 'text': None}
+        paragraphs.append({
+            'globs': globs,
+            'copyright': par.copyright,
+            'comment': par.comment,
+            'license': l})
+    return paragraphs
+
+
+def parse_licenses_for_html_render(copyright):
+    """ Creates list of licenses with urls
+    """
+    licenses = []
+    for par in copyright.all_license_paragraphs():
+        licenses.append({'synopsis': par.license.synopsis,
+                         'link': match_license(par.license.synopsis),
+                         'text': par.license.text,
+                         'comment': par.comment})
+    return licenses
+
+
+def create_url(glob="", base=None,):
+    # don't create links for hidden folders/files
+    if base is None or not re.search('^\.', glob):
+        if glob.count('*') > 0:
+            # find deepest folder without *
+            parts = glob.split('/')
+            index = [parts.index(part) for part in parts
+                     if '*' in part][0]
+            url = base + '/'.join(parts[0:index])
+        else:
+            url = base + glob
+        return url
+    else:
+        return None
+
+
+def match_license(synopsis):
+    """ Matches a `synopsis` with a license and creates a url
+    """
+    key = filter(lambda x: re.search(x, synopsis) is not None, Licenses)
+    if len(key) is not 0:
+        return Licenses[key[0]]
+    else:
+        return None
+
+
+def parse_license_synopsis(copyright, synopsis):
+    """ Parses a license and created links to license texts
+
+    """
+    license = []
+    if any(keyword in synopsis for keyword in ['and', 'or']):
+        licenses = re.split('(, | ?and | ?or )', synopsis)
+        for l in licenses:
+            link = match_license(l)
+            if not link:
+                license.append([l, anchor_to_license(copyright, l)])
+            else:
+                license.append([l, link])
+    else:
+        link = match_license(synopsis)
+        if not link:
+            license.append([synopsis, anchor_to_license(copyright, synopsis)])
+        else:
+            license.append([synopsis, link])
+    return license
+
+
+def anchor_to_license(copyright, synopsis):
+    """ Matches license into a license in the licenses paragraphs and
+        creates an anchor link there.
+
+    """
+    licenses = [par.license.synopsis
+                for par in copyright.all_license_paragraphs()]
+    if synopsis in licenses:
+        return '#license-' + str(licenses.index(synopsis))
     else:
         return None
